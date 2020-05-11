e53f46325dbc4ab8f2ee0d05650442f37ca864d2
Kevin Power <kpower@cerner.com>
2020-05-11 09:59:57 -0500

    Update to EA output file names
    Remove the 'corrected' file name with the plan to always use the 'EAFullModelNative' file going forward.

--- _genall.sh
+++ _genall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo running Transform
-java -jar ./saxon/saxon9he.jar -t -s:resources/EAFullModelNativeCorrected.xml -xsl:resources/XMI2FHIRXML.xsl
+java -jar ./saxon/saxon9he.jar -t -s:resources/EAFullModelNative.xml -xsl:resources/XMI2FHIRXML.xsl
 
 echo building IG
 cd ig
